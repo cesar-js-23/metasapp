034c78f7f07625fa1c8ccc4a55119216fe8b9e4c
Cesar Julian <cejulian23@gmail.com>
2022-07-22 22:46:27 -0500

    install tailwind

---
+++ src/componentes/compartidos/Pie.js
@@ -0,0 +1,10 @@
+function Pie() {
+    return (
+      <footer className="text-green-500">   
+          <small>Desarrollo y diseñado por Cesar Julian Salazar 2030, Todos los derechos reservados.</small>
+      </footer>
+    );
+  }
+  
+  export default Pie;
+  
